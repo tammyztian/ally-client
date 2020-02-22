e4be8556337b13ff55f78aed5b545f626826b639
Wennona <50216937+WWWeLLs@users.noreply.github.com>
2020-02-22 13:13:38 -0600

    landing page

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import App from './components/app';
+import AllyLandingPage from './components/AllyLandingPage';
+// import App from './components/app';
 import store from './store';
 import './index.css';
 
 ReactDOM.render(
     <Provider store={store}>
         <Router>
-            <App />
+            <AllyLandingPage />
         </Router>
     </Provider>,
     document.getElementById('root')
